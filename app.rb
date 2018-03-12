5de265bfd45d380e675d56468c73828007b164c1
lidianvilazio <lidianvilazio@hotmail.com>
2018-03-11 22:07:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Lidian"
+  end
+  get '/hometown' do
+    "My hometown is Rio de Janeiro,"
+  end
+  get '/favorite-song' do
+    "My favorite song is Little Walter My Babe."
+  end
 end
