14a9b940e5fb715b1ada9a9ccaa137358c693e9f
Donnovan Lyons <dlyons8@binghamton.edu>
2018-03-13 09:51:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get('/name') {"My name is Donnovan"}
+
+  get('/hometown') {"My hometown is Hempstead"}
+
+  get '/favorite-song' do
+    "My favorite song is Ophelia"
+  end
+
 end
