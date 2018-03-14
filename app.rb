a8bf7445917b9b42fe46c599a403e42f371a999e
Barney Fabian <bafa@tutanota.de>
2018-03-14 21:03:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,18 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    "Hello, World!"
+    'Hello, World!'
   end
   
   get '/name' do 
-    "My name is __"
+    'My name is '
   end
   
   get '/hometown' do
-    "My hometown is __"
+    'My hometown is '
   end
   
   get '/favorite-song' do 
-    "My favorite song is __"
+    'My favorite song is '
   end
 end
